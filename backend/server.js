4e6e25cf865e116c892a220caa8fbabe2f449912
Erick William <erikcwill@gmail.com>
2023-10-02 18:15:43 -0300

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,74 +1,44 @@
-const express = require("express");
-const { MongoClient, ObjectID } = require("mongodb");
-const cors = require("cors"); 
+// backend/server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors()); 
 app.use(express.json());
+app.use(cors());
 
-const uri = "mongodb+srv://erikcwill:DWipznRYfU8Lm3Rf@databases.xpmudft.mongodb.net/databases?retryWrites=true&w=majority";
-
-const client = new MongoClient(uri, {
+mongoose.connect("mongodb+srv://erikcwill:DWipznRYfU8Lm3Rf@databases.xpmudft.mongodb.net/databases?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-async function connectToDatabase() {
-  try {
-    await client.connect();
-    console.log("Conectado ao MongoDB Atlas");
-  } catch (err) {
-    console.error("Erro ao conectar ao MongoDB Atlas:", err);
-  }
-}
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const database = client.db("databases");
-    const collection = database.collection("products");
-    const products = await collection.find({}).toArray();
-    res.json(products);
-  } catch (err) {
-    console.error("Erro ao buscar produtos:", err);
-    res.status(500).send("Erro interno do servidor");
-  }
+const ProductSchema = new mongoose.Schema({
+  name: String,
 });
 
-app.post("/api/products", async (req, res) => {
-  try {
-    const { name } = req.body;
-    const database = client.db("databases");
-    const collection = database.collection("products");
-    const result = await collection.insertOne({ name });
-    const newProduct = result.ops[0];
-    console.log("Novo produto adicionado:", newProduct); // Adicione esta linha para depuração
-    res.json(newProduct);
-  } catch (err) {
-    console.error("Erro ao adicionar produto:", err);
-    res.status(500).send("Erro interno do servidor");
-  }
+const Product = mongoose.model('Product', ProductSchema);
+
+app.get('/api/products', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
 });
 
-app.delete("/api/products/:id", async (req, res) => {
-  try {
-    const productId = req.params.id;
-    const database = client.db("databases");
-    const collection = database.collection("products");
-    const result = await collection.deleteOne({ _id: ObjectID(productId) });
-    if (result.deletedCount === 1) {
-      res.sendStatus(200);
-    } else {
-      res.status(404).send("Produto não encontrado");
-    }
-  } catch (err) {
-    console.error("Erro ao remover produto:", err);
-    res.status(500).send("Erro interno do servidor");
-  }
+app.post('/api/products', async (req, res) => {
+  const { name } = req.body;
+  const product = new Product({ name });
+  await product.save();
+  res.json(product);
 });
 
-connectToDatabase();
+app.delete('/api/products/:id', async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndRemove(id);
+  res.json({ message: 'Product removed successfully' });
+});
 
-app.listen(port, () => {
-  console.log(`Servidor está rodando na porta ${port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- src/components/ProductManager.js
+++ src/components/ProductManager.js
@@ -1,49 +1,46 @@
+// src/components/ProductManagement.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ProductManager = () => {
+const ProductManagement = () => {
   const [products, setProducts] = useState([]);
   const [newProduct, setNewProduct] = useState('');
 
-  // Fetch products from the backend on component mount
   useEffect(() => {
     axios.get('http://localhost:5000/api/products')
       .then((response) => {
         setProducts(response.data);
       })
       .catch((error) => {
-        console.error('Error fetching products:', error);
+        console.error(error);
       });
   }, []);
 
-  // Function to add a new product
-  const addProduct = () => {
+  const handleAddProduct = () => {
     axios.post('http://localhost:5000/api/products', { name: newProduct })
       .then((response) => {
-        console.log("Resposta do servidor após adicionar produto:", response.data); // Adicione esta linha para depuração
         setProducts([...products, response.data]);
         setNewProduct('');
       })
       .catch((error) => {
-        console.error('Error adding product:', error);
+        console.error(error);
       });
   };
 
-  // Function to remove a product
-  const removeProduct = (productId) => {
-    console.log("ID do produto a ser removido:", productId); // Adicione esta linha para depuração
+  const handleRemoveProduct = (productId) => {
     axios.delete(`http://localhost:5000/api/products/${productId}`)
       .then(() => {
-        setProducts(products.filter(product => product._id !== productId));
+        setProducts(products.filter((product) => product._id !== productId));
       })
       .catch((error) => {
-        console.error('Error removing product:', error);
+        console.error(error);
       });
   };
 
   return (
     <div>
-      <h1>Product Manager</h1>
+      <h1>Product Management</h1>
       <div>
         <input
           type="text"
@@ -51,13 +48,13 @@ const ProductManager = () => {
           value={newProduct}
           onChange={(e) => setNewProduct(e.target.value)}
         />
-        <button onClick={addProduct}>Add Product</button>
+        <button onClick={handleAddProduct}>Add Product</button>
       </div>
       <ul>
         {products.map((product) => (
           <li key={product._id}>
             {product.name}
-            <button onClick={() => removeProduct(product._id)}>Remove</button>
+            <button onClick={() => handleRemoveProduct(product._id)}>Remove</button>
           </li>
         ))}
       </ul>
@@ -65,4 +62,4 @@ const ProductManager = () => {
   );
 };
 
-export default ProductManager;
+export default ProductManagement;
