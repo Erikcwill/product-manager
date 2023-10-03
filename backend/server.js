5b87f152c862ffec7b0c5e2686a77472bf4224b2
Erick William <erikcwill@gmail.com>
2023-10-03 06:28:59 -0300

    update on backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,10 @@ mongoose.connect("mongodb+srv://erikcwill:DWipznRYfU8Lm3Rf@databases.xpmudft.mon
 
 const ProductSchema = new mongoose.Schema({
   name: String,
+  price: Number, // Novo campo para preço
+  category: String, // Novo campo para categoria
 });
+
 
 const Product = mongoose.model('Product', ProductSchema);
 
@@ -26,19 +29,41 @@ app.get('/api/products', async (req, res) => {
 });
 
 app.post('/api/products', async (req, res) => {
-  const { name } = req.body;
-  const product = new Product({ name });
-  await product.save();
-  res.json(product);
+  const { name, price, category } = req.body;
+
+  // Verifique se o nome não está vazio
+  if (!name || name.trim() === '') {
+    return res.status(400).json({ error: 'O campo "Nome" é obrigatório.' });
+  }
+
+  // Verifique se o preço é um número válido
+  if (isNaN(price) || price < 0) {
+    return res.status(400).json({ error: 'O campo "Preço" deve ser um número válido maior ou igual a zero.' });
+  }
+
+  // Verifique se a categoria não está vazia
+  if (!category || category.trim() === '') {
+    return res.status(400).json({ error: 'O campo "Categoria" é obrigatório.' });
+  }
+
+  const product = new Product({ name, price, category });
+
+  try {
+    await product.save();
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Ocorreu um erro ao adicionar o produto.' });
+  }
 });
 
 app.delete('/api/products/:id', async (req, res) => {
   const { id } = req.params;
   await Product.findByIdAndRemove(id);
-  res.json({ message: 'Product removed successfully' });
+  res.json({ message: 'Produto removido com sucesso' });
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`O servidor está rodando na porta ${PORT}`);
 });
--- src/components/ProductManager.js
+++ src/components/ProductManager.js
@@ -1,11 +1,11 @@
-// src/components/ProductManagement.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ProductManagement = () => {
   const [products, setProducts] = useState([]);
   const [newProduct, setNewProduct] = useState('');
+  const [newPrice, setNewPrice] = useState('');
+  const [newCategory, setNewCategory] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/products')
@@ -18,10 +18,16 @@ const ProductManagement = () => {
   }, []);
 
   const handleAddProduct = () => {
-    axios.post('http://localhost:5000/api/products', { name: newProduct })
+    axios.post('http://localhost:5000/api/products', {
+      name: newProduct,
+      price: parseFloat(newPrice.replace(',', '.')), // Formate o preço para um número com ponto decimal
+      category: newCategory,
+    })
       .then((response) => {
         setProducts([...products, response.data]);
         setNewProduct('');
+        setNewPrice('');
+        setNewCategory('');
       })
       .catch((error) => {
         console.error(error);
@@ -40,21 +46,35 @@ const ProductManagement = () => {
 
   return (
     <div>
-      <h1>Product Management</h1>
+      <h1>Gerenciamento de Produtos</h1>
       <div>
         <input
           type="text"
-          placeholder="Enter product name"
+          placeholder="Digite o nome do produto"
           value={newProduct}
           onChange={(e) => setNewProduct(e.target.value)}
         />
-        <button onClick={handleAddProduct}>Add Product</button>
+        <input
+          type="text"
+          placeholder="Digite o preço do produto (exemplo: 10,59)"
+          value={newPrice}
+          onChange={(e) => setNewPrice(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Digite a categoria do produto"
+          value={newCategory}
+          onChange={(e) => setNewCategory(e.target.value)}
+        />
+        <button onClick={handleAddProduct}>Adicionar Produto</button>
       </div>
       <ul>
         {products.map((product) => (
           <li key={product._id}>
-            {product.name}
-            <button onClick={() => handleRemoveProduct(product._id)}>Remove</button>
+            <strong>Nome:</strong> {product.name}<br />
+            <strong>Preço:</strong> R$ {parseFloat(product.price).toLocaleString('pt-BR', { minimumFractionDigits: 2 })}<br />
+            <strong>Categoria:</strong> {product.category}<br />
+            <button onClick={() => handleRemoveProduct(product._id)}>Remover</button>
           </li>
         ))}
       </ul>
